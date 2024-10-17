379ac55aa778dd32bfefaa1c6dde79681611521c
Andrii Serdiuk <andreyserdjuk1@gmail.com>
2024-10-17 21:56:23 +0300

    Created specific repository-classes

---
+++ FileStorage/FileStorage.DAL/Repositories/StoredFileRepository.cs
@@ -0,0 +1,16 @@
+﻿using FileStorage.DAL.Data;
+using FileStorage.DAL.Models;
+using FileStorage.DAL.Repositories.Interfaces;
+
+namespace FileStorage.DAL.Repositories
+{
+	public class StoredFileRepository : Repository<StoredFile>, IStoredFileRepository
+	{
+		private readonly FileStorageDbContext _context;
+
+		public StoredFileRepository(FileStorageDbContext context) : base(context)
+		{
+			_context = context;
+		}
+	}
+}
---
+++ FileStorage/FileStorage.DAL/Repositories/UserRepository.cs
@@ -0,0 +1,16 @@
+﻿using FileStorage.DAL.Data;
+using FileStorage.DAL.Models;
+using FileStorage.DAL.Repositories.Interfaces;
+
+namespace FileStorage.DAL.Repositories
+{
+	public class UserRepository : Repository<User>, IUserRepository
+	{
+		private readonly FileStorageDbContext _context;
+
+		public UserRepository(FileStorageDbContext context) : base(context)
+		{
+			_context = context;
+		}
+	}
+}
