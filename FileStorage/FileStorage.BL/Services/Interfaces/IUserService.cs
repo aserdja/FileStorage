fed8a45e7916a239d1705fbf960157f34542832f
Andrii Serdiuk <andreyserdjuk1@gmail.com>
2024-11-02 18:56:02 +0200

    Added base realization of UserService

---
+++ FileStorage/FileStorage.BL/Services/Interfaces/IUserService.cs
@@ -0,0 +1,7 @@
+﻿namespace FileStorage.BL.Services.Interfaces
+{
+	public interface IUserService
+	{
+		Task<bool> RegisterUserAsync(string name, string login, string email, string password, string passwordConfirmation);
+	}
+}
---
+++ FileStorage/FileStorage.BL/Services/UserService.cs
@@ -0,0 +1,39 @@
+﻿using FileStorage.BL.Models;
+using FileStorage.BL.Services.Interfaces;
+using FileStorage.DAL.Models;
+using FileStorage.DAL.UnitOfWork;
+
+namespace FileStorage.BL.Services
+{
+	public class UserService(IUnitOfWork unitOfWork, IUserValidationService userValidationService) : IUserService
+	{
+		private readonly IUnitOfWork _unitOfWork = unitOfWork;
+		private readonly IUserValidationService userValidationService = userValidationService;
+
+		public async Task<bool> RegisterUserAsync(string name, string login, string email, string password, string passwordConfirmation)
+		{
+			var newUser = new UserRegistration { Name = name, Login = login, Email = email, Password = password, PasswordConfirmation = passwordConfirmation };
+
+			if (userValidationService.ValidateUserCredentials(newUser) != null)
+			{
+				_unitOfWork.Users.Add(ConvertToUser(newUser));
+				
+				await _unitOfWork.CommitAsync();
+				return true;
+			}
+
+			return false;
+		}
+
+		private User ConvertToUser(UserRegistration userRegistrationModel)
+		{
+			return new User
+			{
+				Name = userRegistrationModel.Name,
+				Login = userRegistrationModel.Login,
+				Email = userRegistrationModel.Email,
+				Password = userRegistrationModel.Password,
+			};
+		}
+	}
+}
