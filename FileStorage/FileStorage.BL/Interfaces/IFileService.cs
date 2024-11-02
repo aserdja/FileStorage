2d1f7a1db90b9429a4590c6df88632f26c5ce08a
i4ibob <i4ibob@gmail.com>
2024-11-02 19:58:19 +0200

    011 BL FileService added method SaveFileLocalAsync

--- FileStorage/FileStorage.BL/Interfaces/IFileService.cs
+++ FileStorage/FileStorage.BL/Interfaces/IFileService.cs
@@ -1,6 +1,10 @@
-﻿namespace FileStorage.BL.Interfaces
+﻿using FileStorage.DAL.Models;
+using Microsoft.AspNetCore.Http;
+namespace FileStorage.BL.Interfaces
 {
     internal interface IFileService
     {
+
+        Task SaveFileLocalAsync(IFormFile file, User user, bool isPublic);
     }
 }
--- FileStorage/FileStorage.BL/Services/FileService.cs
+++ FileStorage/FileStorage.BL/Services/FileService.cs
@@ -7,10 +7,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using FileStorage.DAL.Repositories.Interfaces;
+using FileStorage.DAL.Models;
+using FileStorage.BL.Interfaces;
+using Microsoft.AspNetCore.Http;
 
 namespace FileStorage.BL.Services
 {
-    public class FileService
+    public class FileService : IFileService
     {
         private readonly IUnitOfWork _unitOfWork;
         private readonly IStoredFileRepository _storedFileRepository;
@@ -22,5 +25,55 @@ namespace FileStorage.BL.Services
             _storedFileDetailsRepository = storedFileDetailsRepository;
         }
 
+
+        public async Task SaveFileLocalAsync(IFormFile file, User user, bool isPublic)
+        {
+            // Определяем путь для хранения файла
+            var rootUploadsPath = Path.Combine(Directory.GetCurrentDirectory(), "Uploads");
+            var userFolderPath = Path.Combine(rootUploadsPath, user.Name); // Можно использовать также user.Id
+            var targetFolder = isPublic ? "Public" : "Private";
+            var targetFolderPath = Path.Combine(userFolderPath, targetFolder);
+
+            // Создаём папку, если её ещё нет
+            if (!Directory.Exists(targetFolderPath))
+            {
+                Directory.CreateDirectory(targetFolderPath);
+            }
+
+            // Полный путь для сохранения файла
+            var filePath = Path.Combine(targetFolderPath, file.FileName);
+
+            // Сохраняем файл в файловую систему
+            using (var stream = new FileStream(filePath, FileMode.Create))
+            {
+                await file.CopyToAsync(stream);
+            }
+
+            // Заполняем данные для StoredFile
+            var storedFile = new StoredFile
+            {
+                Name = file.FileName,
+                Type = file.ContentType,
+                Size = file.Length / 1024.0 / 1024.0, // Размер в мегабайтах
+                Path = filePath,
+                UserId = user.Id
+            };
+
+            // Заполняем данные для StoredFileDetails
+            var storedFileDetails = new StoredFileDetails
+            {
+                IsPublic = isPublic,
+                UploadDateTime = DateTime.UtcNow,
+                ExpireDateTime = DateTime.UtcNow.AddYears(1), // Условная дата истечения
+                StoredFile = storedFile
+            };
+
+            // Сохраняем данные в базу данных
+            await _unitOfWork.StoredFiles.CreateAsync(storedFile);
+            await _unitOfWork.StoredFilesDetails.CreateAsync(storedFileDetails);
+
+
+            await _unitOfWork.CommitAsync();
+        }
     }
 }
