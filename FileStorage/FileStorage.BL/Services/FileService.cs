d00eacce77489414b08b4613c94ff460e16b22a8
i4ibob <i4ibob@gmail.com>
2024-11-02 19:20:47 +0200

    010 BL to FileSrevice conected UnitOfWork StoredFileRepository , StoredFileDetailsRepository

--- FileStorage/FileStorage.BL/Services/FileService.cs
+++ FileStorage/FileStorage.BL/Services/FileService.cs
@@ -1,12 +1,26 @@
-﻿using System;
+﻿using FileStorage.DAL.UnitOfWork;
+using FileStorage.DAL;
+using FileStorage.DAL.Repositories;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using FileStorage.DAL.Repositories.Interfaces;
 
 namespace FileStorage.BL.Services
 {
     public class FileService
     {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IStoredFileRepository _storedFileRepository;
+        private readonly IStoredFileDetailsRepository _storedFileDetailsRepository;
+        public FileService(IStoredFileRepository storedFileRepository, IStoredFileDetailsRepository storedFileDetailsRepository, IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+            _storedFileRepository = storedFileRepository;
+            _storedFileDetailsRepository = storedFileDetailsRepository;
+        }
+
     }
 }
