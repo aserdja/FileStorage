cfad275b6738e2468f7eb56ebd5db8c0d5eda92b
Andrii Serdiuk <andreyserdjuk1@gmail.com>
2024-11-06 00:31:58 +0200

    Created Extension class. Moved services

---
+++ FileStorage/FileStorage.WebApi/Extensions/ServiceExtensions.cs
@@ -0,0 +1,39 @@
+﻿using FileStorage.BL.Services.Interfaces;
+using FileStorage.BL.Services;
+using FileStorage.DAL.Repositories.Interfaces;
+using FileStorage.DAL.Repositories;
+using FileStorage.DAL.UnitOfWork;
+using FileStorage.DAL.Data;
+using Microsoft.AspNetCore.Authentication.Cookies;
+
+namespace FileStorage.WebApi.Extensions
+{
+	public static class ServiceExtensions
+	{
+		public static void AddApplicationServices(this IServiceCollection services)
+		{
+			services.AddScoped<IUserRepository, UserRepository>();
+			services.AddScoped<IStoredFileRepository, StoredFileRepository>();
+			services.AddScoped<IStoredFileDetailsRepository, StoredFileDetailsRepository>();
+			services.AddScoped<IUnitOfWork, UnitOfWork>();
+			services.AddScoped<IUserValidationService, UserValidationService>();
+			services.AddScoped<IUserService, UserService>();
+		}
+
+		public static void AddDbContext(this IServiceCollection services)
+		{
+			services.AddDbContext<FileStorageDbContext>();
+		}
+
+		public static void ConfigureAuthentication(this IServiceCollection services)
+		{
+			services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+				.AddCookie(option =>
+				{
+					option.LoginPath = "/api/user/signin";
+					option.LogoutPath = "/api/user/signout";
+					option.ExpireTimeSpan = TimeSpan.FromMinutes(30);
+				});
+		}
+	}
+}
--- FileStorage/FileStorage.WebApi/Program.cs
+++ FileStorage/FileStorage.WebApi/Program.cs
@@ -4,9 +4,11 @@ using FileStorage.DAL.Data;
 using FileStorage.DAL.Repositories;
 using FileStorage.DAL.Repositories.Interfaces;
 using FileStorage.DAL.UnitOfWork;
+using FileStorage.WebApi.Extensions;
 using Microsoft.AspNetCore.Authentication.Cookies;
 
 var builder = WebApplication.CreateBuilder(args);
+var configuration = builder.Configuration;
 
 // Add services to the container.
 
@@ -14,22 +16,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddAuthentication(
-	CookieAuthenticationDefaults.AuthenticationScheme)
-	.AddCookie(option => 
-	{
-		option.LoginPath = "/api/user/signin";
-		option.LogoutPath = "/api/user/signout";
-		option.ExpireTimeSpan = TimeSpan.FromMinutes(30);
-	});
-
-builder.Services.AddDbContext<FileStorageDbContext>();
-builder.Services.AddScoped<IUserRepository, UserRepository>();
-builder.Services.AddScoped<IStoredFileRepository, StoredFileRepository>();
-builder.Services.AddScoped<IStoredFileDetailsRepository, StoredFileDetailsRepository>();
-builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
-builder.Services.AddScoped<IUserValidationService, UserValidationService>();
-builder.Services.AddScoped<IUserService, UserService>();
+builder.Services.AddDbContext();
+builder.Services.AddApplicationServices();
+builder.Services.ConfigureAuthentication();
 
 
 var app = builder.Build();
