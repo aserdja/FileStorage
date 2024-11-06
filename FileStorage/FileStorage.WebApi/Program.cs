6b00e158564782f26aa6f7053fc0f5a7682fac02
Andrii Serdiuk <andreyserdjuk1@gmail.com>
2024-11-06 02:39:36 +0200

    Cleared unnecessary usings

--- FileStorage/FileStorage.WebApi/Program.cs
+++ FileStorage/FileStorage.WebApi/Program.cs
@@ -1,11 +1,4 @@
-using FileStorage.BL.Services;
-using FileStorage.BL.Services.Interfaces;
-using FileStorage.DAL.Data;
-using FileStorage.DAL.Repositories;
-using FileStorage.DAL.Repositories.Interfaces;
-using FileStorage.DAL.UnitOfWork;
 using FileStorage.WebApi.Extensions;
-using Microsoft.AspNetCore.Authentication.Cookies;
 
 var builder = WebApplication.CreateBuilder(args);
 var configuration = builder.Configuration;
@@ -26,8 +19,8 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+	app.UseSwagger();
+	app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
