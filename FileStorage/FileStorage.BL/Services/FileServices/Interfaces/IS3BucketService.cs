ffefe8de52cd815ff6db3d1c344b510dc32583ed
Andrii Serdiuk <andreyserdjuk1@gmail.com>
2024-11-07 02:30:33 +0200

    Created S3BucketService

---
+++ FileStorage/FileStorage.BL/Services/FileServices/Interfaces/IS3BucketService.cs
@@ -0,0 +1,9 @@
+﻿using FileStorage.BL.Models;
+
+namespace FileStorage.BL.Services.FileServices.Interfaces
+{
+	public interface IS3BucketService
+	{
+		Task<bool> UploadFileToS3BucketAsync(FileUploading fileUploading, string currentUserEmail);
+	}
+}
