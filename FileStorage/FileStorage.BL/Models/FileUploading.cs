c45f75b7a261c5a95ef654f2df67e489a242daaa
Andrii Serdiuk <andreyserdjuk1@gmail.com>
2024-11-07 02:29:06 +0200

    Created model for file uploading

---
+++ FileStorage/FileStorage.BL/Models/FileUploading.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FileStorage.BL.Models
+{
+	public class FileUploading
+	{
+		[Required]
+		public string FileName { get; set; } = string.Empty;
+
+		[Required]
+		public string ContentType {  get; set; } = string.Empty;
+
+		[Required]
+		public string S3Path { get; set; } = string.Empty;
+
+		[Required]
+		public long Length { get; set; }
+
+		[Required]
+		public Stream Stream { get; set; } = null!;
+	}
+}
