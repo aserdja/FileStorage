c8a498232deac45b4f1f782c4e5f61a00b272130
Andrii Serdiuk <andreyserdjuk1@gmail.com>
2024-11-08 19:02:53 +0100

    Added UserRepositoryIntegrationTests

---
+++ FileStorage/FileStorage.Tests/UserRepositoryIntegrationTests.cs
@@ -0,0 +1,50 @@
+﻿using FileStorage.DAL.Data;
+using FileStorage.DAL.Models;
+using FileStorage.DAL.Repositories;
+using FileStorage.DAL.Repositories.Interfaces;
+
+namespace FileStorage.Tests
+{
+	public class UserRepositoryIntegrationTests
+	{
+		[Fact]
+		public async Task GetByEmailAsync_expects_get_user()
+		{
+			var context = new FileStorageDbContext();
+			IUserRepository userRepository = new UserRepository(context);
+			var email = "User1@user.test";
+			var user = new User { Name = "User1", Email = email, Login = "Test_user_1", Password = "123123asd" };
+			context.Add(user);
+			await context.SaveChangesAsync();
+
+
+			var result = await userRepository.GetByEmailAsync(email);
+
+
+			Assert.Equal(user, result);
+
+			context.Remove(user);
+			await context.SaveChangesAsync();
+		}
+
+		[Fact]
+		public async Task GetByLoginAsync_expects_get_user()
+		{
+			var context = new FileStorageDbContext();
+			IUserRepository userRepository = new UserRepository(context);
+			var login = "login@test";
+			var user = new User { Name = "User1", Email = "test@test.test", Login = login, Password = "123123asd" };
+			context.Add(user);
+			await context.SaveChangesAsync();
+
+
+			var result = await userRepository.GetByLoginAsync(login);
+
+
+			Assert.Equal(user, result);
+
+			context.Remove(user);
+			await context.SaveChangesAsync();
+		}
+	}
+}
