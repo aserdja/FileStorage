67160cbf6e295a8ec0b1260ab3a1e01d0a9cb6e1
Andrii Serdiuk <andreyserdjuk1@gmail.com>
2024-11-08 01:52:03 +0100

    Changed GetFiles to return DTO collection

--- FileStorage/FileStorage.WebApi/Controllers/StoredFileController.cs
+++ FileStorage/FileStorage.WebApi/Controllers/StoredFileController.cs
@@ -1,8 +1,11 @@
-﻿using FileStorage.BL.Models;
+﻿using AutoMapper;
+using FileStorage.BL.Models;
 using FileStorage.BL.Services.FileServices.Interfaces;
 using FileStorage.BL.Services.UserServices.Interfaces;
+using FileStorage.DAL.Models;
 using FileStorage.DAL.Repositories.Interfaces;
 using FileStorage.DAL.UnitOfWork;
+using FileStorage.WebApi.DTOs;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System.Security.Claims;
@@ -12,10 +15,11 @@ namespace FileStorage.WebApi.Controllers
 	[Authorize]
 	[Route("api/[controller]")]
 	[ApiController]
-	public class StoredFileController(IFileService fileService, IUnitOfWork unitOfWork) : Controller
+	public class StoredFileController(IFileService fileService, IUnitOfWork unitOfWork, IMapper mapper) : Controller
 	{
 		private readonly IFileService _fileService = fileService;
 		private readonly IUnitOfWork _unitOfWork = unitOfWork;
+		private readonly IMapper _mapper = mapper;
 
 		[HttpPost]
 		public async Task<IActionResult> UploadFile(IFormFile file)
@@ -56,15 +60,23 @@ namespace FileStorage.WebApi.Controllers
 
 			try
 			{
-				var storedFilesCollection = _fileService.GetFilesByEmailAsync(currentUserEmail);
-				var storedFilesDetailsCollection = _unitOfWork.StoredFilesDetails.GetAllByStoredFilesAsync(await storedFilesCollection);
-
+				var storedFilesCollection = _fileService.GetFilesByEmailAsync(currentUserEmail).Result.ToList();
+				var storedFilesDetailsCollection = _unitOfWork.StoredFilesDetails.GetAllByStoredFilesAsync(storedFilesCollection).Result.ToList();
+				List<StoredFileDetailsDTO> result = new();
+				
 				if (storedFilesDetailsCollection == null)
 				{
 					Ok("You haven't uploaded files to storage yet");
 				}
 
-				return Ok();
+				for (int i = 0; i < storedFilesCollection.Count; i++)
+				{
+					var storedFileDetailsDTO = _mapper.Map<StoredFileDetailsDTO>(storedFilesCollection[i]);
+					var mappedModel = _mapper.Map(storedFilesDetailsCollection[i], storedFileDetailsDTO);
+					result.Add(mappedModel);
+				}
+				
+				return Ok(result);
 			}
 			catch (Exception)
 			{
